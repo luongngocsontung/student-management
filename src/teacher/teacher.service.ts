87501ca3f0d19919322a676399f1213fc0bdd42d
Luong Tung <luongngocsontung@gmail.com>
2025-02-12 21:28:03 +0700

    Register students

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { RegisterStudentRequestDTO } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,6 +6,27 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class TeacherService {
   constructor(private prisma: PrismaService) {}
   async registerStudent(dto: RegisterStudentRequestDTO) {
-    // console.log(await this.prisma.teacher.findMany());
+    try {
+      const teacher = await this.prisma.teacher.update({
+        where: { email: dto.teacher },
+        data: {
+          students: {
+            connect: [...dto.students.map((student) => ({ email: student }))],
+          },
+        },
+      });
+      return teacher;
+    } catch (error) {
+      if (error.code === 'P2016') {
+        throw new HttpException('Teacher not found', HttpStatus.NOT_FOUND);
+      }
+      if (error.code === 'P2025') {
+        throw new HttpException(
+          '1 or more students not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      throw error;
+    }
   }
 }

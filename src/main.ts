6d8dc2572c19f744716db299f864628060508cb4
Luong Tung <luongngocsontung@gmail.com>
2025-02-12 21:52:06 +0700

    White list the redundant request body

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = app.get(ConfigService);
   const port = config.get<number>('port') ?? 3000;
